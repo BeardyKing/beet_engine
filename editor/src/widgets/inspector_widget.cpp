8ef1f8f48855528d07478532a7fdedf939335eb9
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2022-05-10 20:52:12 +0100

    transform widget rendering
    have gimble lock :(

--- editor/src/widgets/inspector_widget.cpp
+++ editor/src/widgets/inspector_widget.cpp
@@ -1,5 +1,6 @@
 #include "inspector_widget.h"
 #include <beet/scene.h>
+#include <imgui.h>
 
 namespace beet {
 
@@ -7,15 +8,20 @@ InspectorWidget::InspectorWidget(const std::string& name, EditorWidgets& editorW
     : Widget(name), m_editorWidgets(editorWidgets) {}
 
 void InspectorWidget::on_widget_render() {
-    using namespace components;
-    auto selected = m_editorWidgets.get_selected_entity();
-
     ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
     ImGui::Begin(m_name.c_str());
 
+    render_inspector();
+
+    ImGui::End();
+}
+
+void InspectorWidget::render_inspector() {
+    using namespace components;
+    auto selected = m_editorWidgets.get_selected_entity();
+
     auto sceneOpt = Scene::get_active_scene();
     if (!sceneOpt) {
-        ImGui::End();
         return;
     }
 
@@ -24,7 +30,6 @@ void InspectorWidget::on_widget_render() {
 
     auto goOpt = scene.get_game_object_from_id(selected);
     if (!goOpt) {
-        ImGui::End();
         return;
     }
 
@@ -33,20 +38,56 @@ void InspectorWidget::on_widget_render() {
     m_handle = std::to_string(static_cast<size_t>(go.get_handle()));
     m_uuid = uuids::to_string(go.get_id());
 
+    //=Render Inspector===========
+
     auto name = registry.try_get<components::Name>(go.get_handle());
     if (name) {
         render_name_component(*name);
     }
+
     ImGui::Separator();
 
-    ImGui::End();
+    auto transform = registry.try_get<components::Transform>(go.get_handle());
+    if (transform) {
+        render_transform_component(*transform);
+    }
+
+    ImGui::Separator();
 }
+
 void InspectorWidget::render_name_component(components::Name& name) {
     char entityName[64]{};
     sprintf_s(entityName, "%s", name.name.c_str());
 
     ImGui::InputText("##INSPECTOR_NAME", entityName, IM_ARRAYSIZE(entityName));
     name.name = entityName;
+}
+void InspectorWidget::render_transform_component(components::Transform& transform) {
+    ImGui::Separator();
+    if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_AllowItemOverlap)) {
+        vec3 position = transform.get_position();
+        vec3 rotation = transform.get_rotation_euler();
+        vec3 scale = transform.get_scale();
+
+        ImGui::Text("Transform");
+
+        ImGui::Text("Position : ");
+        ImGui::SameLine();
+        ImGui::DragFloat3("##P", &position.x, -0.1f, 0.1f);
+
+        ImGui::Text("Rotation : ");
+        ImGui::SameLine();
+        ImGui::DragFloat3("##R", &rotation.x, -1.0f, 1.0f);
+
+        ImGui::Text("Scale :    ");
+        ImGui::SameLine();
+        ImGui::DragFloat3("##S", &scale.x, -0.1f, 0.1f);
+
+        transform.set_position(position);
+        transform.set_rotation_euler(rotation);
+        transform.set_scale(scale);
+    }
+    ImGui::Separator();
 }
 
 InspectorWidget::~InspectorWidget() {
--- editor/src/widgets/inspector_widget.h
+++ editor/src/widgets/inspector_widget.h
@@ -14,7 +14,10 @@ class InspectorWidget : public Widget {
     void on_widget_render() override;
 
    private:
+    void render_inspector();
     void render_name_component(components::Name& name);
+    void render_transform_component(components::Transform& transform);
+    void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f);
 
    private:
     EditorWidgets& m_editorWidgets;
